cbe05a36e9e27265f37d589f8ef73fcedf34584b
Stenu <sami.stenvall@student.tut.fi>
2016-09-20 20:11:23 +0300

    testi2

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,38 +1,53 @@
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
- 
+
   This example code is in the public domain.
- */
- 
+*/
+
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int led = 13;
+int led1 = 13;
 int led2 = 12;
 int led3 = 11;
 
-int ledit[3][8] = 
-  {
-    {1,},
-    {1,},
-    {1,}    
-  };
+int ledit[3] = {led1, led2, led3};
+
+int luvut[8][3] =
+{
+  {1, 1, 1},
+  {1, 1, 0},
+  {1, 0, 1},
+  {1, 0, 0},
+  {0, 1, 1},
+  {0, 1, 0},
+  {0, 0, 1},
+  {0, 0, 0}
+};
 
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() {
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
 }
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(led2,HIGH);
-  digitalWrite(led3, HIGH);
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(led2,LOW);
-  digitalWrite(led3,LOW);
-  delay(1000);               // wait for a second
+
+  for (int i = 0; i < 8; i++) {
+
+    for (int j = 0; j < 3; j++) {
+
+      if (luvut[i][j] == 1) {
+        digitalWrite(ledit[j], HIGH);
+      } else {
+        digitalWrite(ledit[j], LOW);
+      }
+
+    }
+    delay(1000);
+  }
+
+  
 }
